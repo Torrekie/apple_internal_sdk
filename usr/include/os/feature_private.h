c2b0fb3f1267234258fb1d4a8ed7783d1cdd7aa3
Torrekie <me@torrekie.dev>
2022-03-23 18:50:39 +0800

    add feature_private.h

---
+++ usr/include/os/feature_private.h
@@ -0,0 +1,30 @@
+#ifndef _OS_FEATURE_PRIVATE_H_
+#define _OS_FEATURE_PRIVATE_H_
+
+#include <stdbool.h>
+#include <os/base.h>
+
+#pragma clang assume_nonnull begin
+
+/**
+ * While calling os_feature_enabled(domain, feature),
+ * /usr/lib/system/libsystem_featureflags.dylib searches
+ * plist files under /System/Library/FeatureFlags/Domain
+ * and /Library/Preferences/FeatureFlags/Domain with name
+ * defined by 'domain'.
+ * (e.g. domain = objc4 goes objc4.plist)
+ * 'feature' refers to a Dictionary value containing a bool
+ * value in domain plist file.
+ */
+
+#define os_feature_enabled(d, f) _os_feature_enabled_impl(#d, #f)
+#define os_feature_enabled_simple(d, f, s) _os_feature_enabled_simple_impl(#d, #f, #s)
+
+__BEGIN_DECLS
+
+bool _os_feature_enabled_impl(const char *domain, const char *feature);
+bool _os_feature_enabled_simple_impl(const char *domain, const char *feature, bool status);
+
+__END_DECLS
+
+#endif
