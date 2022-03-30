dd8ca4ba4c0556a1dbf5a49eaa683795996f49c2
Torrekie <me@torrekie.dev>
2022-03-31 00:43:13 +0800

    liboah and libCrashReporterClient

---
+++ misc/CrashReporterClient/CrashReporterClient.c
@@ -0,0 +1,6 @@
+#include <CrashReporterClient.h>
+
+CRASH_REPORTER_CLIENT_HIDDEN
+struct crashreporter_annotations_t gCRAnnotations
+    __attribute__((section("__DATA," CRASHREPORTER_ANNOTATIONS_SECTION)))
+    = { CRASHREPORTER_ANNOTATIONS_VERSION, 0, 0, 0, 0, 0, 0, 0 };
