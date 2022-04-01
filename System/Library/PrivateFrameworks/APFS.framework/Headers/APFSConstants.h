da234265ce269144176efe801f6da47904d1d3b6
Torrekie <me@torrekie.dev>
2022-04-01 18:39:26 +0800

    value fixes

--- System/Library/PrivateFrameworks/APFS.framework/Headers/APFSConstants.h
+++ System/Library/PrivateFrameworks/APFS.framework/Headers/APFSConstants.h
@@ -3,11 +3,17 @@
 
 #import <CoreFoundation/CoreFoundation.h>
 
+#define apple_apfs 0x1200
+
+#define APFS_BUNDLE_ID "com.apple.filesystems.apfs"
+
 #define APFS_VOL_ROLE_DATA 64
 
-#define EDT_OS_ENV_MAIN 2
+#define EDT_OS_ENV_MAIN 1
+#define EDT_OS_ENV_OTHER 2
 #define EDT_OS_ENV_DIAGS 3
 
+#define EDTVolumeFSType			"apfs"
 #define EDTVolumePropertySize		(2 << 4)
 #define EDTVolumePropertyMaxSize	(2 << 7) // Guessed
 
