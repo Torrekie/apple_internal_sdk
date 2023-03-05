de8abe198c04079fdb11695cf6bfaf8a29e432ad
Torrekie <me@torrekie.dev>
2023-03-05 16:20:18 +0800

    Why section missing while linking?

--- misc/CrashReporterClient/CrashReporterClient.c
+++ misc/CrashReporterClient/CrashReporterClient.c
@@ -2,5 +2,5 @@
 
 CRASH_REPORTER_CLIENT_HIDDEN
 struct crashreporter_annotations_t gCRAnnotations
-    __attribute__((section("__DATA," CRASHREPORTER_ANNOTATIONS_SECTION)))
+    __attribute__((used, section("__DATA," CRASHREPORTER_ANNOTATIONS_SECTION)))
     = { CRASHREPORTER_ANNOTATIONS_VERSION, 0, 0, 0, 0, 0, 0, 0 };
