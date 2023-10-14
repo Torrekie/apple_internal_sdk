aa9528b6036738ee59cb2cc5ad94987e1da6e665
Torrekie <31795847+Torrekie@users.noreply.github.com>
2023-10-14 19:34:09 +0800

    kLAPolicyContinuityUnlock

--- System/Library/Framework/LocalAuthentication.framework/Headers/LAPrivateDefines.h
+++ System/Library/Framework/LocalAuthentication.framework/Headers/LAPrivateDefines.h
@@ -4,10 +4,14 @@
 #include <LocalAuthentication/LAPublicDefines.h>
 
 /* Tue Feb 21 19:39:04 CST 2023
- * Reversed from /usr/lib/pam/libtid.so.2 in macOS 11.2.3
+ * Reversed from /usr/lib/pam/pam_tid.so.2 in macOS 11.2.3
  */
 
+/*
+ * Reversed from /usr/lib/pam/pam_localauthentication.so.2 in macOS 11.2.3
+ */
 #define kLAOptionNotInteractive					1000
+#define kLAPolicyContinuityUnlock				1012
 #define kLAOptionUserId						1019
 
 // Not sure which header these func should be at
