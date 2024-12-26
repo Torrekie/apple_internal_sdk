7d9420cbb044b29724de84d37c85912c95cb1097
Torrekie <31795847+Torrekie@users.noreply.github.com>
2024-12-26 14:58:50 +0800

    AppleFDEKeyStore

---
+++ System/Library/Framework/IOKit.framework/Headers/crypto/AppleFDEKeyStoreDefs.h
@@ -0,0 +1,34 @@
+#ifndef _IOKIT_APPLEFDEKEYSTOREDEFS_H
+#define _IOKIT_APPLEFDEKEYSTOREDEFS_H
+
+#include <CoreFoundation/CoreFoundation.h>
+
+#define kAppleFDEKeyStoreServiceName "AppleFDEKeyStore"
+
+typedef CF_ENUM(UInt32, FDEAlgorithm)
+{
+	fDE_ALG_AESXTS = 2,
+};
+
+typedef struct createKeyGetUUID_InStruct
+{
+	UInt32 keySizeInBytes;
+	FDEAlgorithm algorithm;
+} createKeyGetUUID_InStruct_t;
+
+typedef struct uuid_OutStruct
+{
+	uuid_t uuid;
+} uuid_OutStruct_t;
+
+typedef CF_ENUM(UInt8, FDEIndex)
+{
+	kAppleFDEKeyStoreUserClientOpen,
+	kAppleFDEKeyStoreUserClientClose
+
+	kAppleFDEKeyStore_createKeyGetUUID = 27,
+};
+
+#define V1_KEYSIZE sizeof(uuid_OutStruct_t);
+
+#endif
