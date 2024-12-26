27af4fcc8e4152c6a73e8eb1c286839cafed56ed
Torrekie <31795847+Torrekie@users.noreply.github.com>
2024-12-26 15:30:08 +0800

    xts

--- System/Library/Framework/IOKit.framework/Headers/crypto/AppleFDEKeyStoreDefs.h
+++ System/Library/Framework/IOKit.framework/Headers/crypto/AppleFDEKeyStoreDefs.h
@@ -10,25 +10,36 @@ typedef CF_ENUM(UInt32, FDEAlgorithm)
 	fDE_ALG_AESXTS = 2,
 };
 
-typedef struct createKeyGetUUID_InStruct
+typedef struct createKeyGetUUID
 {
 	UInt32 keySizeInBytes;
 	FDEAlgorithm algorithm;
 } createKeyGetUUID_InStruct_t;
 
-typedef struct uuid_OutStruct
+typedef struct
 {
 	uuid_t uuid;
 } uuid_OutStruct_t;
+
+#define V1_KEYSIZE sizeof(uuid_OutStruct_t);
+
+#define XTS_TWEAK_BYTES 16
+
+typedef struct xtsEncrypt
+{
+	uint64_t bufferAddress;
+	uint64_t bufferLength;
+	uint8_t tweak[XTS_TWEAK_BYTES]; /* 16-bit little-endian bytes */
+	uuid_t key_uuid;
+} xtsEncrypt_InStruct_t;
 
 typedef CF_ENUM(UInt8, FDEIndex)
 {
 	kAppleFDEKeyStoreUserClientOpen,
 	kAppleFDEKeyStoreUserClientClose,
 
-	kAppleFDEKeyStore_createKeyGetUUID = 27,
+	kAppleFDEKeyStore_createKeyGetUUID	= 27,
+	kAppleFDEKeyStore_xtsEncrypt		= 29,
 };
-
-#define V1_KEYSIZE sizeof(uuid_OutStruct_t);
 
 #endif
